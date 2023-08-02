5408038324a1d09b54e6d8c2a3bcce85a9f4d370
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-02 15:06:24 -0400

    song update action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -18,4 +18,14 @@ class SongsController < ApplicationController
     )
     render :show
   end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    @song.title = params[:title] || @song.title
+    @song.album = params[:album] || @song.album
+    @song.artist = params[:artist] || @song.artist
+    @song.year = params[:year] || @song.year
+    @song.save
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
   get "/songs" => "songs#index"
   get "/songs/:id" => "songs#show"
   post "/songs" => "songs#create"
+  patch "/songs/:id" => "songs#update"
 end
