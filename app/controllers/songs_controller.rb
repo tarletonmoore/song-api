7c5dbcf8130b38698a204d8378850c578f49e8a7
Tarleton Moore <tarletonmoore@Tarletons-MacBook-Pro.local>
2023-08-02 14:51:14 -0400

    song show action

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,4 +3,9 @@ class SongsController < ApplicationController
     @songs = Song.all
     render :index
   end
+
+  def show
+    @song = Song.find_by(id: params[:id])
+    render :show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/songs" => "songs#index"
+  get "/songs/:id" => "songs#show"
 end
